de5c6ae502b48e1bddd93a6deed06c9c8d6ca0fa
João Gabriel Campos <joaaogabriel.oi@hotmail.com>
2022-05-13 02:45:32 -0300

    feat: create page cars

---
+++ src/app/presentation/pages/cars/cars.tsx
@@ -0,0 +1,13 @@
+import { ContentContainer, RootContainer } from './cars-styles'
+
+const CarsPage = () => {
+  return (
+    <RootContainer>
+      <ContentContainer>
+        <h1>OI</h1>
+      </ContentContainer>
+    </RootContainer>
+  )
+}
+
+export default CarsPage
